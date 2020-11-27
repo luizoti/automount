ce7864b032ae44c4282253e812b27af21bc730bb
luizoti <luizlazoti@hotmail.com>
2020-11-27 12:41:49 -0300

    added funcs to get device name, and changes in all methods, now only need to pass key

--- domount.py
+++ domount.py
@@ -3,21 +3,35 @@
 import re
 import os
 import dbus
-import os.path
+from os.path import join, realpath
 #
 METHOD = {
     "fs": "org.freedesktop.UDisks2.Filesystem",
     "pt": "org.freedesktop.UDisks2.PartitionTable",
+    "canpoweroff": "org.freedesktop.UDisks2.Drive.CanPowerOff",
+    "poweroff": "org.freedesktop.UDisks2.Drive.PowerOff",
+    "ejectable": "org.freedesktop.UDisks2.Drive.Ejectable",
+    "eject": "org.freedesktop.UDisks2.Drive.Eject",
+}
+
+PATHS = {
+    "drives": "/org/freedesktop/UDisks2/drives",
+    "block_devices": "/org/freedesktop/UDisks2/block_devices/",
 }
 
 class UDisksBus():
     def init(self, DEV, method):
+        if method in ['fs', 'pt']:
+            path = PATHS['block_devices']
+        elif method in ['canpoweroff', 'poweroff', 'ejectable', 'eject']:
+            path = PATHS['drives']
+            pass
+
         bus = dbus.SystemBus()
-        OBJ = bus.get_object('org.freedesktop.UDisks2',
-            ''.join(['/org/freedesktop/UDisks2/block_devices/', DEV]))
+        OBJ = bus.get_object('org.freedesktop.UDisks2', ''.join([path, DEV]))
 
         try:
-            return dbus.Interface(OBJ, dbus_interface=method)
+            return dbus.Interface(OBJ, dbus_interface=METHOD[method])
         except Exception as e:
             raise e
 
@@ -29,12 +43,23 @@ class UDisksBus():
             return False
         return True
 
+    def getDevicePath(self, device):
+        DISKS_PATH = "/dev/disk/by-id/"
 
-    def partitionTableChecker(self, DEV):
-        status = False
+        for devicename in os.listdir(DISKS_PATH):
+            devicepath = join(DISKS_PATH, devicename)
+            if device in realpath(devicepath):
+                print(device)
+                print(re.sub(r'(usb|ata|wwn)\-', '', re.sub(r'\-([a-z].*|[0,99]\:.+?)', '', devicename)))
+                pass
+            pass
+
+
+    def partitionTableChecker(self, deviceslist):
         devs = []
 
-        for device in DEV:
+        for device in deviceslist:
+            status = False
             if device[-1].isdigit():
                 devs.append(device)
             else:
@@ -42,7 +67,7 @@ class UDisksBus():
                     devpath = ''.join(["/dev/", device, str(x)])
 
                     if self.devExists(devpath) == True:
-                        devs.append(''.join([device, str(x)]))
+                        devs.append(os.path.basename(devpath))
                         status = True
 
                     if x == 10 and status == False:
@@ -52,9 +77,9 @@ class UDisksBus():
         return devs
 
 
-    def mount(self, DEV):
-        for device in self.partitionTableChecker(DEV):
-            init = self.init(device, METHOD['fs'])
+    def mount(self, devices):
+        for device in self.partitionTableChecker(devices):
+            init = self.init(device, 'fs')
             domount = init.get_dbus_method('Mount')
 
             try:
@@ -67,7 +92,6 @@ class UDisksBus():
 
                 if "AlreadyMounted" in expt:
                     yield 'already', device, re.findall(r"\`(.+?)\'", str(expt))[0]
-                    pass
 
 
     def umount(self, DEV):
@@ -78,7 +102,7 @@ class UDisksBus():
             pass
 
         for device in self.partitionTableChecker(_devs):
-            init = self.init(device, METHOD['fs'])
+            init = self.init(device, 'fs')
             doumount = init.get_dbus_method('Unmount')
 
             try:
@@ -86,7 +110,7 @@ class UDisksBus():
             except Exception as e:
                 expt = str(e)
                 if "AlreadyMounted" in expt:
-                    yield 'already', device, re.findall(r"\`(.+?)\'", mntOut)[0]
+                    yield '', device, re.findall(r"\`(.+?)\'", mntOut)[0]
                     pass
                 
 
@@ -95,7 +119,7 @@ class UDisksBus():
         for device in self.partitionTableChecker(DEV):
             self._umount() # You need to unmount before repairing the disc.
 
-            init = self.init(device, METHOD['fs'])
+            init = self.init(device, 'fs')
             repair = init.get_dbus_method('Repair')
 
             try:
@@ -105,13 +129,15 @@ class UDisksBus():
 
 
     # def resize(self, DEV):
-    #     return self.init(DEV, METHOD['fs']).get_dbus_method('Resize')({'s': 'a'})
+    #     return self.init(DEV, 'fs').get_dbus_method('Resize')({'s': 'a'})
 
     # def setlabel(self, DEV):
-    #     return self.init(DEV, METHOD['fs']).get_dbus_method('SetLabel')('NEEWW')
+    #     return self.init(DEV, 'fs').get_dbus_method('SetLabel')('NEEWW')
 
     # def ownership(self, DEV):
     #     try:
-    #         return self.init(DEV, METHOD['fs']).get_dbus_method('TakeOwnership')({'s': 'a'})
+    #         return self.init(DEV, 'fs').get_dbus_method('TakeOwnership')({'s': 'a'})
     #     except Exception as e:
     #         print(e)
+
+UDisksBus().getDevicePath("sdc")
